4debbd5ca3fda5f0e06996d83b8e1f28660e74d6
Julian Nico Pielmaier <94388978+Jaypee2109@users.noreply.github.com>
2024-01-12 14:33:41 +0100

    generate weights

--- wp1.py
+++ wp1.py
@@ -16,24 +16,15 @@ def read_pickle(filepath):
 
 
 def reduce_dicograph(dicograph, factor, special=False):
-    edgeTuple = graph_to_rel(dicograph)
-    print("VORHER----------------------------", edgeTuple)
+    edgeTuple_dicograph = graph_to_rel(dicograph)
 
-    reduce_graph(edgeTuple, factor, special)
+    reduce_graph(edgeTuple_dicograph, factor, special)
 
-    print("NACHHER-------------------------", edgeTuple)
-
-    return edgeTuple
+    return edgeTuple_dicograph
 
 
 def reduce_graph(edgeTuple, factor, special=False):
-    print("original: ", edgeTuple)
-    numOfEdges = count_edges(edgeTuple)
-    print("number of all edges: ", numOfEdges)
-
     for key in edgeTuple:
-        print(key, " set:")
-
         edgeList = edgeTuple[key]
 
         if special and key == "d":
@@ -44,19 +35,19 @@ def reduce_graph(edgeTuple, factor, special=False):
 
             while numOfDeletes > 0:
                 deletedElement = edgeList.pop(random.randint(0, len(edgeList) - 1))
-                print(deletedElement)
                 numOfDeletes -= 1
 
                 symmetricTuple = (deletedElement[1], deletedElement[0])
 
                 if symmetricTuple in edgeList:
-                    print("symmetric deleted: ", end="")
-                    print(symmetricTuple)
                     edgeList.remove(symmetricTuple)
                     numOfDeletes -= 1
 
-    print("number of all edges: ", count_edges(edgeTuple))
-    print("reduced: ", edgeTuple)
+    print("Percentage deleted        - ", factor * 100)
+    print("Special scenario          - ", special)
+    print("Empty relations           - ", edgeTuple[0])
+    print("Directed relations        - ", edgeTuple["d"])
+    print("Bidirected relations      - ", edgeTuple[1])
 
     return edgeTuple
 
@@ -125,30 +116,83 @@ def generate_di_cograph(nodes):
     return dicograph
 
 
+def create_weights(nodes, relations, edge_value, non_edge_value=0):
+    weights = {"uni_weighted": {}, "bi_weighted": {}, "empty_weighted": {}}
+
+    for node in nodes:
+        for pairNode in nodes:
+            if node == pairNode:
+                continue
+
+            relationTuple = (node, pairNode)
+
+            if relationTuple in relations[0]:
+                weights["empty_weighted"][relationTuple] = edge_value
+            else:
+                weights["empty_weighted"][relationTuple] = non_edge_value
+
+            if relationTuple in relations[1]:
+                weights["bi_weighted"][relationTuple] = edge_value
+            else:
+                weights["bi_weighted"][relationTuple] = non_edge_value
+
+            if relationTuple in relations["d"]:
+                weights["uni_weighted"][relationTuple] = edge_value
+            else:
+                weights["uni_weighted"][relationTuple] = non_edge_value
+
+    return weights
+
+
 if __name__ == "__main__":
+    path = "graph-prak-GFH/n25/D0.5_L0.5_H0.25/D0.5_L0.5_H0.25_n25_14"
+
+    nodeset_graph = nx.read_graphml(path + "/dFitch.graphml", int).nodes
     edgeTuple = {1: [], "d": [], 0: []}
 
     # print("bidirect: ", end="")
-    edgeTuple[1] = read_pickle(
-        "graph-prak-GFH/n25/D0.5_L0.5_H0.25/D0.5_L0.5_H0.25_n25_14/biRelations.pkl"
-    )
+    edgeTuple[1] = read_pickle(path + "/biRelations.pkl")
 
     # print("unidirect: ", end="")
-    edgeTuple["d"] = read_pickle(
-        "graph-prak-GFH/n25/D0.5_L0.5_H0.25/D0.5_L0.5_H0.25_n25_14/uniRelations.pkl"
-    )
+    edgeTuple["d"] = read_pickle(path + "/uniRelations.pkl")
 
     # print("empty: ", end="")
-    edgeTuple[0] = read_pickle(
-        "graph-prak-GFH/n25/D0.5_L0.5_H0.25/D0.5_L0.5_H0.25_n25_14/emptyRelations.pkl"
-    )
+    edgeTuple[0] = read_pickle(path + "/emptyRelations.pkl")
+
+    print()
+    print("XENOLOGY GRAPH")
+    print("Nodeset                   - ", nodeset_graph)
+    print("Empty relations           - ", edgeTuple[0])
+    print("Directed relations        - ", edgeTuple["d"])
+    print("Bidirected relations      - ", edgeTuple[1])
+    print("----------------------------")
 
     # task 2 & 2b
-    # reduce_graph(edgeTuple, 0.5, False)
+    partial_graph = reduce_graph(edgeTuple, 0.9)
 
     # task 3
-    nodes = [i for i in range(5)]
-    random_dicograph = generate_di_cograph(nodes)
+    nodeset_dicograph = [i for i in range(10)]
+    random_dicograph = generate_di_cograph(nodeset_dicograph)
+    edgeTuple_dicograph = graph_to_rel(random_dicograph)
+
+    print()
+    print("DI-COGRAPH")
+    print("Nodeset                   - ", nodeset_dicograph)
+    print("Empty relations           - ", edgeTuple_dicograph[0])
+    print("Directed relations        - ", edgeTuple_dicograph["d"])
+    print("Bidirected relations      - ", edgeTuple_dicograph[1])
+    print("----------------------------")
 
     # task 4
-    reduce_dicograph(random_dicograph, 0.5)
+    partial_dicograph = reduce_dicograph(random_dicograph, 0.9)
+
+    # task 5
+    fitch_cotree_graph_012 = algorithm_one(partial_graph, nodeset_graph, (0, 1, 2))
+    fitch_cotree_dicograph_012 = algorithm_one(
+        partial_dicograph, nodeset_dicograph, (0, 1, 2)
+    )
+
+    print(create_weights(nodeset_dicograph, partial_dicograph, 100))
+
+    fitch_relations_graph_greedy = algorithm_two
+    fitch_relations_dicograph__greedy = algorithm_two
